940e541de563c999690766c25f38069aefb122ac
celinalopez <celina.lopez2799@gmail.com>
2023-11-14 22:45:15 -0300

    Revert "primer commit"
    This reverts commit 0f15cc5a547d258357f2efd10cff69bfdd8aced8.

--- src/main/java/utn/proyectoContable/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package utn.proyectoContable.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import utn.proyectoContable.model.PlanDeCuentas;
-
-public interface TodoRepository extends JpaRepository<PlanDeCuentas, Integer> {
-}
