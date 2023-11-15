8d0545c1127b4da00152813d78fb21790c63ac00
celinalopez <celina.lopez2799@gmail.com>
2023-11-14 23:26:52 -0300

    update

--- src/main/java/utn/proyectoContable/model/Mayor.java
+++ src/main/java/utn/proyectoContable/model/Mayor.java
@@ -1,10 +1,9 @@
 package utn.proyectoContable.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "mayores")
 public class Mayor {
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
